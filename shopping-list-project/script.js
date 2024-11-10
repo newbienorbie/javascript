36f8fc099b027119350ded5771ee3db54228e69f
Norberta Sandrine <151909800+newbienorbie@users.noreply.github.com>
2024-11-10 13:58:03 +0800

    project finished

--- shopping-list-project/script.js
+++ shopping-list-project/script.js
@@ -1,8 +1,19 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear");
+const itemFilter = document.getElementById("filter");
+let isEditMode = false;
+const formBtn = itemForm.querySelector("button");
 
-function addItem(e) {
+function displayItems() {
+  const itemsFromStorage = getItemsFromStorage();
+  itemsFromStorage.forEach((item) => addItemToDOM(item));
+
+  checkUI();
+}
+
+function onAddItemSubmit(e) {
   e.preventDefault(); // to prevent the form being submitted to the file
 
   const newItem = itemInput.value;
@@ -13,17 +24,44 @@ function addItem(e) {
     return; // if there's nothing there then just return
   }
 
+  // check for edit mode: take the item that we're editing, remove it from the DOM and localstorage, add the new item
+  if (isEditMode) {
+    const itemToEdit = itemList.querySelector(".edit-mode");
+
+    removeItemFromStorage(itemToEdit.textContent);
+    itemToEdit.classList.remove("edit-mode");
+    itemToEdit.remove();
+    isEditMode = false;
+  } else {
+    if (checkIfItemExists(newItem)) {
+      alert(`${newItem} already exists!`);
+      return;
+    }
+  }
+
+  // create item DOM element
+  addItemToDOM(newItem);
+
+  // add item to local storage
+  addItemToStorage(newItem);
+
+  // runs to check whether there's an item added
+  checkUI();
+
+  itemInput.value = "";
+}
+
+function addItemToDOM(item) {
   // create list item
   const li = document.createElement("li");
-  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(item));
 
   const button = createButton("remove-item btn-link text-red");
 
   li.appendChild(button);
 
+  // adding li to  dom
   itemList.appendChild(li);
-
-  itemInput.value = "";
 }
 
 function createButton(classes) {
@@ -40,5 +78,141 @@ function createIcon(classes) {
   return icon;
 }
 
-// event listeners
-itemForm.addEventListener("submit", addItem);
+function addItemToStorage(item) {
+  let itemsFromStorage = getItemsFromStorage();
+
+  // add new item to array
+  itemsFromStorage.push(item);
+
+  // convert the array to json string and set to local storage
+  localStorage.setItem("items", JSON.stringify(itemsFromStorage));
+}
+
+function getItemsFromStorage() {
+  let itemsFromStorage;
+
+  // checking if there's any item in the storage
+  if (localStorage.getItem("items") === null) {
+    itemsFromStorage = [];
+  } else {
+    // parse the string back to array
+    itemsFromStorage = JSON.parse(localStorage.getItem("items"));
+  }
+
+  return itemsFromStorage;
+}
+
+function onClickItem(e) {
+  // make sure we're clicking on the X
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    removeItem(e.target.parentElement.parentElement);
+  } else {
+    setItemToEdit(e.target);
+  }
+}
+
+function checkIfItemExists(item) {
+  const itemsFromStorage = getItemsFromStorage();
+
+  return itemsFromStorage.includes(item);
+}
+
+function setItemToEdit(item) {
+  isEditMode = true;
+
+  itemList
+    .querySelectorAll("li")
+    .forEach((i) => i.classList.remove("edit-mode"));
+  // add css if hover over item
+  item.classList.add("edit-mode");
+  formBtn.innerHTML = '<i class="fa-solid fa-pen"></i>   Update Item';
+  formBtn.style.backgroundColor = "#228B22";
+  itemInput.value = item.textContent;
+}
+
+function removeItem(item) {
+  if (confirm("Are you sure?")) {
+    // remove item from DOM
+    item.remove();
+
+    // remove item from storage
+    removeItemFromStorage(item.textContent);
+
+    checkUI();
+  }
+}
+
+function removeItemFromStorage(item) {
+  let itemsFromStorage = getItemsFromStorage();
+
+  // filter out item to be removed
+  // filter will return a new array with the deleted item removed
+  itemsFromStorage = itemsFromStorage.filter((i) => i !== item);
+
+  // reset to localstorage
+  localStorage.setItem("items", JSON.stringify(itemsFromStorage));
+}
+
+function clearItems() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+
+  // clear from local storage
+  localStorage.removeItem("items");
+
+  checkUI();
+}
+
+function filterItems(e) {
+  const items = itemList.querySelectorAll("li");
+  const text = e.target.value.toLowerCase();
+
+  // if using older methods like getElementById, it will return HTMLCollection which has to be converted into array first to use forEach
+  // if using newer methods like querySelector, it will return NodeList so we can use forEach straightaway
+
+  items.forEach((item) => {
+    // access the text in li tag
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    // checks whether there's any character in itemName that matches with text
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
+function checkUI() {
+  itemInput.value = "";
+
+  // the variable needs to be declared here. if it's in the global scope, the length will always be 0 because it only runs once
+  const items = itemList.querySelectorAll("li");
+  if (items.length === 0) {
+    clearBtn.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    clearBtn.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+  formBtn.innerHTML = '<i class = "fa-solid fa-plus"></i> Add Item';
+  formBtn.style.backgroundColor = "#333";
+
+  isEditMode = false;
+}
+
+// initialize app
+function init() {
+  // event listeners
+  itemForm.addEventListener("submit", onAddItemSubmit);
+  itemList.addEventListener("click", onClickItem);
+  clearBtn.addEventListener("click", clearItems);
+  itemFilter.addEventListener("input", filterItems);
+  document.addEventListener("DOMContentLoaded", displayItems);
+
+  // it only runs when the page loads so even if we add items, the filter and clear button won't show up because we didn't load the page
+  checkUI();
+}
+
+init();
